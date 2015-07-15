4f252d5db9bb3f91f242691e06daa716c5d545b1
Ben Creasy <contact@bencreasy.com>
2015-07-14 23:36:39 -0700

    Tweak gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var taskList = require('gulp-task-listing');
+var spawn = require('child_process').spawn;
 
-
-gulp.task('default', taskList.withFilters(/:/, 'default'))
+gulp.task('dev', function() {
+  spawn('nodemon', ['./server/app.js'], {stdio: [0, 1, 2]});
+  
+})
+gulp.task('default', taskList.withFilters(/:/, 'default'));
